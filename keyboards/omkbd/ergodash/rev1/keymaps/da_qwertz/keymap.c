50a4cf64009f87ead0f732fef3123e936f38a22d
Daniel <daniel.t.altvater@gmail.com>
2022-08-21 17:30:35 +0200

    use quantum/keymap_extras/keymap_german.h

--- keyboards/omkbd/ergodash/rev1/keymaps/da_qwertz/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/da_qwertz/keymap.c
@@ -1,7 +1,8 @@
 #include QMK_KEYBOARD_H
 
+#include "quantum/keymap_extras/keymap_german.h"
 
-#define _QWERTY 0
+#define _QWERTZ 0
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 16
@@ -30,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | Ctrl |  GUI |  ALt | EISU |||||||| Lower| Space|  Del |||||||| Bksp | Enter| Raise|||||||| Left | Down |  Up  | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
-  [_QWERTY] = LAYOUT(
+  [_QWERTZ] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                        KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
@@ -116,7 +117,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case QWERTY:
       if (record->event.pressed) {
          print("mode just switched to qwerty and this is a huge string\n");
-        set_single_persistent_default_layer(_QWERTY);
+        set_single_persistent_default_layer(_QWERTZ);
       }
       return false;
       break;
